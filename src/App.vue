8f229c8a6927997dea36009999c509c0880cd17f
murtuzzov <murtuzzov>
2026-03-13 01:11:39 +0400

    Исправил кнопки корзины и поиска в App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="min-h-screen pb-20 md:pb-8 flex flex-col">
-    <TheHeader />
+    <!-- Добавил обработчики событий -->
+    <TheHeader
+      @open-cart="isCartOpen = true"
+      @open-search="isSearchOpen = true"
+    />
 
     <router-view class="flex-grow" @open-battle="isBattleOpen = true" />
 
@@ -18,6 +22,9 @@
       @close="isBattleOpen = false"
     />
 
+    <!-- Добавил SearchModal (его не было в шаблоне!) -->
+    <SearchModal :is-open="isSearchOpen" @close="isSearchOpen = false" />
+
     <CartDrawer :is-open="isCartOpen" @close="isCartOpen = false" />
   </div>
 </template>
@@ -28,8 +35,10 @@ import TheHeader from "./components/layout/TheHeader.vue";
 import MobileBottomBar from "./components/layout/MobileBottomBar.vue";
 import BattleModal from "./components/ui/BattleModal.vue";
 import CartDrawer from "./components/ui/CartDrawer.vue";
-import TheFooter from "./components/layout/TheFooter.vue"; // импорт
+import TheFooter from "./components/layout/TheFooter.vue";
+import SearchModal from "./components/ui/SearchModal.vue"; // ← добавил импорт
 
 const isBattleOpen = ref(false);
 const isCartOpen = ref(false);
+const isSearchOpen = ref(false); // ← добавил состояние
 </script>
